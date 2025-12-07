d2eea0a3f4a87b4a7bb2470acec6ea6b440122f6
mathew <donnj95@gmail.com>
2025-12-07 12:13:55 +0900

    파일입출력과 스레드 내용보강

--- Main.java
+++ Main.java
@@ -1,13 +1,3 @@
-
 void main() {
 
-    int[] nums = new int[] {21, 4, 6, 39, 3, 19, 12};
-
-    int temp = nums[0];
-    nums[0] = nums[1];
-    nums[1] = temp;
-
-    IO.println(Arrays.toString(nums));
 }
-
-
